6b69ac7766f049d39f8c5e5b150b3cb09af617bc
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-12 08:20:53 -0300

    Add multiplication table generation and file creation

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,9 +1,18 @@
+const fs = require('fs');
 
+console.clear();
 
 // 5 x 1 = 5
 // 5 x 2 = 10
 // 5 x 3 = 15
 
+let salida = '';
+
 for (let i = 1; i <= 10; i++) {
-    console.log(`5 x ${i} = ${5 * i}`);
+    salida += `5 x ${i} = ${5 * i}\n`;
 } 
+
+fs.writeFile('tabla-5.txt', salida, (err) => {
+    if (err) throw err;
+    console.log('tabla-5.txt creado!!!');
+}); 
