139a8979e35bd2f62376b3c717cd1bd6a339d776
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-12 07:57:27 -0300

    Add getNombre method to deadpool object

--- 02-fundamentos/desestructuracion.js
+++ 02-fundamentos/desestructuracion.js
@@ -4,4 +4,9 @@ const deadpool = {
     nombre: 'Wade',
     apellido: 'Winston',
     poder: 'Regeneración',
+    getNombre() {
+        return `${this.nombre} ${this.apellido} ${this.poder}`
+    }
 }
+
+console.log(deadpool.getNombre());
