517c0458c468bea9f221db73647fd9ab5fc41330
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-15 11:04:27 -0300

    Update base variable and create multiplication table

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -2,12 +2,13 @@ const { crearArchivoTabla } = require('./helpers/multiplicar');
 
 console.clear();
 
-const [ , , arg3 ] = process.argv;
+const [ , , arg3 = 'base=5'] = process.argv;
+const [ , base = 5] = arg3.split('=');
 
-console.log(arg3);
+console.log(base);
 
-let base = 7;
+// let base = 7;
 
-// crearArchivoTabla(base)
-//     .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
-//     .catch(err => console.log(err));
+crearArchivoTabla(base)
+    .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
+    .catch(err => console.log(err));
