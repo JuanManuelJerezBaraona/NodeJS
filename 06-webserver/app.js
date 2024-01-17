da34542768d0062f9959bc417eb95548e084471c
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-17 11:30:34 -0300

    Add start script and update dependencies

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,12 +1,36 @@
 const express = require('express');
+const hbs = require('hbs');
+require('dotenv').config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Servir contenido estático
 app.use(express.static('public'));
 
-app.get('/otra-ruta', (req, res) => {
-    res.send('Otra ruta');
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'Juan Jerez',
+        titulo: 'Curso de Node'
+    });
+});
+
+app.get('/generic', (req, res) => {
+    res.render('generic', {
+        nombre: 'Juan Jerez',
+        titulo: 'Curso de Node'
+    });
+});
+
+app.get('/elements', (req, res) => {
+    res.render('elements', {
+        nombre: 'Juan Jerez',
+        titulo: 'Curso de Node'
+    });
 });
 
 app.get('*', (req, res) => {
