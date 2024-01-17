3464dd30532464dd7672297c4885313b06164b99
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-17 18:08:16 -0300

    Refactor user controller and routes

--- 07-restserver/controllers/user.js
+++ 07-restserver/controllers/user.js
@@ -1,26 +1,36 @@
 const { response } = require('express');
 
 const userGet = (req, res = response) => {
+
+    const { name = 'no name', apikey, page = 1, limit = 10 } = req.query;
+
     res.json({
-        msg: 'get API - controller'
+        msg: 'get API - controller',
+        name,
+        apikey,
+        page,
+        limit
     });
 };
 
 const userPut = (req, res = response) => {
+
+    const { id } = req.params;
+
     res.json({
-        msg: 'put API - controller'
+        msg: 'put API - controller',
+        id
     });
 };
 
 const userPost = (req, res = response) => {
-    res.json({
-        msg: 'post API - controller'
-    });
-};
 
-const userDelete = (req, res = response) => {
+    const {nombre, edad} = req.body;
+
     res.json({
-        msg: 'delete API - controller'
+        msg: 'post API - controller',
+        nombre,
+        edad
     });
 };
 
@@ -30,10 +40,16 @@ const userPatch = (req, res = response) => {
     });
 };
 
+const userDelete = (req, res = response) => {
+    res.json({
+        msg: 'delete API - controller'
+    });
+};
+
 module.exports = {
     userGet,
     userPut,
     userPost,
-    userDelete,
-    userPatch
+    userPatch,
+    userDelete
 }
